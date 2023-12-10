0251badb0110ce759c60b969ec929fcca248d697
AncientSphnx <151151838+AncientSphnx@users.noreply.github.com>
2023-12-10 12:38:30 +0530

    Update Timer.py

--- Timer.py
+++ Timer.py
@@ -1,4 +1,6 @@
+
 import time,keyboard
+from datetime import datetime
 from math import inf
 from playsound import playsound
 def timer():
@@ -12,13 +14,13 @@ def timer():
                 playsound(r'C:\Users\91829\OneDrive\Desktop\coding\Timer project\Sounds\winfantasia-6912.mp3')
             print(i)
             playsound(r'C:\Users\91829\OneDrive\Desktop\coding\Timer project\Sounds\click-button-140881.mp3')
-            time.sleep(1)
+            time.sleep(0.8)
             i-=1
     except:
         print("An error occured please try again")
 def stopwatch():
     print("Press G and enter to start")
-    print("press s to Reset")
+    print("press s to stop")
     e=input()
     if(e.lower()=='g'):
         i=0
@@ -27,9 +29,30 @@ def stopwatch():
                 print("stopped")
                 break
             else:
-                time.sleep(0.1)
+                time.sleep(0.09)
                 print(round(i,3))
                 i+=0.1
+        input("Press enter to quit")
+def alarm():
+    alarm_time = input("Enter the time of alarm to be set:HH:MM:SS\n")
+    alarm_hour=alarm_time[0:2]
+    alarm_minute=alarm_time[3:5]
+    alarm_seconds=alarm_time[6:8]
+    alarm_period = alarm_time[9:11].upper()
+    print("Setting up alarm..")
+    while True:
+        now = datetime.now()
+        current_hour = now.strftime("%I")
+        current_minute = now.strftime("%M")
+        current_seconds = now.strftime("%S")
+        current_period = now.strftime("%p")
+        if(alarm_period==current_period):
+            if(alarm_hour==current_hour):
+                if(alarm_minute==current_minute):
+                    if(alarm_seconds==current_seconds):
+                        print("Wake Up!")
+                        playsound(r'C:\Users\91829\OneDrive\Desktop\coding\Timer project\Sounds\smartphone_vibrating_alarm_silent-7040.mp3')
+                        break
 op=int(input("enter the one of the options\n1)Timer\n2)Stop watch\n"))
 match op:
     case 1:
